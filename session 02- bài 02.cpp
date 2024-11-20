aa5fe4144b95f20546eab79c332b0543f7daa6be
quangduymanh <quangduymanh4@gmail.com>
2024-11-20 18:48:44 +0700

    upload bài 02

---
+++ session 02- bài 02.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main () {
+//khai bao va khoi tao hai so nguyen
+int a=5 ;
+int b=6 ;
+//tinh tong a va b duoc luu vao trong c
+int c=a+b;
+//in ket qua ra man hinh
+cout <<"tong cua" <<a<<"va"<<b<<"la"<<c<<endl;
+
+return 0;
+}
+
